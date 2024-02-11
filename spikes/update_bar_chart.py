2cedcc77befc3794e4a054196204edad51863ba5
Marc Tremblay <tremblay.marc@bcg.com>
2024-02-11 20:50:32 +0100

    Use multiple threads to solve plot hangs

--- spikes/update_bar_chart.py
+++ spikes/update_bar_chart.py
@@ -3,58 +3,54 @@
 
 import random
 import time
-import matplotlib
 import matplotlib.pyplot as plt
-
-
-matplotlib.use("MacOSX")
-
-# Set up a matplotlib figure for displaying live emotion detection results
-plt.ion()  # Turn on interactive mode for live updates
-
-fig, ax = plt.subplots()
+from matplotlib.animation import FuncAnimation
+import threading
+import queue
 
 EMOTION_LABELS = ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"]
 
+emotions_queue = queue.Queue()
+
+fig, ax = plt.subplots()
 bars = ax.bar(
     EMOTION_LABELS, [0] * 7, color="lightblue"
-)  # Initialize bars for each emotion
-plt.ylim(0, 1)
-plt.ylabel("Confidence")
-plt.title("Real-time Emotion Detection")
-ax.set_xticklabels(EMOTION_LABELS, rotation=45)
+)
 
+def init_chart():
+    plt.ylim(0, 1)
+    plt.ylabel("Confidence")
+    plt.title("Real-time Emotion Detection")
+    ax.set_xticklabels(EMOTION_LABELS, rotation=45)
+    return bars
 
-def update_chart(detected_emotions, bars, ax, fig):
-    print(detected_emotions)
-    # Clear the current axes and set up the bar chart again
-    # ax.clear()
-    # ax.bar(
-    #     EMOTION_LABELS,
-    #     [detected_emotions.get(emotion, 0) for emotion in EMOTION_LABELS],
-    #     color="lightblue",
-    # )
-    # plt.ylim(0, 1)
-    # plt.ylabel("Confidence")
-    # plt.title("Real-time Emotion Detection")
-    # ax.set_xticklabels(EMOTION_LABELS, rotation=45)
-    for bar, emotion in zip(bars, EMOTION_LABELS):
-        bar.set_height(detected_emotions.get(emotion, 0))
-    if fig.stale:
-        fig.canvas.draw()
-    fig.canvas.flush_events()
+def update_chart(_frame):
+    if not emotions_queue.empty():
+        detected_emotions = emotions_queue.get()
 
+        for bar, emotion in zip(bars, EMOTION_LABELS):
+            bar.set_height(detected_emotions.get(emotion, 0))
+    return bars
 
-while True:
-    current_emotions = {
-        "angry": random.random(),
-        "disgust": random.random(),
-        "fear": random.random(),
-        "happy": random.random(),
-        "sad": random.random(),
-        "surprise": random.random(),
-        "neutral": random.random(),
-    }
-    update_chart(current_emotions, bars, ax, fig)
+def detect_emotions():
+    while True:
+        detected_emotions = {
+            "angry": random.random(),
+            "disgust": random.random(),
+            "fear": random.random(),
+            "happy": random.random(),
+            "sad": random.random(),
+            "surprise": random.random(),
+            "neutral": random.random(),
+        }
 
-    time.sleep(1)
+        emotions_queue.put(detected_emotions)
+
+        time.sleep(0.01)
+
+thread = threading.Thread(target=detect_emotions)
+thread.start()
+
+ani = FuncAnimation(fig, update_chart, init_func=init_chart, blit=True)
+
+plt.show()
